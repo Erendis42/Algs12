167d0bfa3bd9a1c457f1634946b4041d0539c12c
Erendis Near <mailbox.novaka@gmail.com>
2020-02-19 13:01:23 +0100

    Update Solver.java

--- Algs1/04_8-puzzle/Solver.java
+++ Algs1/04_8-puzzle/Solver.java
@@ -61,12 +61,6 @@ public class Solver {
       for (Board b : generateUsefulNeighbors(minTwin)) {
         nodes.insert(new SearchNode(b, minTwin.getMoves() + 1, minTwin));
       }
-
-      /*
-       * If more than one neighbor has the same lowest priority, in 2 levels of recursion we can
-       * decide whether it is the single way to keep getting lower hamming distances or it starts
-       * rising again.
-       */
     }
   }
 
